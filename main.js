e32d0ff913e126b510b32f900c21984e45d6edef
KALABRIA-KLB <kalabriatoken@gmail.com>
2025-07-12 10:07:29 +0200

    Upload sito web

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
 
-let web3;
-let contractAddress = "0x3F7F7bC596D0494506993e38471894Af7bC41628";
-let contractABI = [
-    {"inputs":[{"internalType":"address","name":"_klb","type":"address"},{"internalType":"uint256","name":"_startTime","type":"uint256"},{"internalType":"uint256","name":"_endTime","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},
-    {"inputs":[],"name":"buyTokens","outputs":[],"stateMutability":"payable","type":"function"},
-    {"inputs":[],"name":"withdrawRemainingTokens","outputs":[],"stateMutability":"nonpayable","type":"function"}
-];
-
-window.addEventListener('load', async () => {
-    if (typeof window.ethereum !== 'undefined') {
-        web3 = new Web3(window.ethereum);
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        document.getElementById("walletStatus").innerText = "Wallet connesso ✅";
-    } else {
-        document.getElementById("walletStatus").innerHTML = "<a href='https://metamask.io/'>Installa MetaMask</a>";
+async function connectWallet() {
+  if (typeof window.ethereum !== 'undefined') {
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      alert("Wallet connesso: " + accounts[0]);
+    } catch (err) {
+      console.error("Errore nella connessione al wallet", err);
     }
-});
-
-async function buyTokens() {
-    const amount = document.getElementById("bnbAmount").value;
-    const accounts = await web3.eth.getAccounts();
-    const contract = new web3.eth.Contract(contractABI, contractAddress);
-    await contract.methods.buyTokens().send({ from: accounts[0], value: web3.utils.toWei(amount, "ether") });
+  } else {
+    alert("MetaMask non trovato");
+  }
 }
