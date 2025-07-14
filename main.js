9c6ec960b4e4fa39605aa7196513108ef35a0ea5
KALABRIA-KLB <kalabriatoken@gmail.com>
2025-07-14 19:18:22 +0200

    Sostituisci file corrotti

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import Web3Modal from "web3modal";
-
 let web3;
 let provider;
 let account;
 
 const providerOptions = {
   walletconnect: {
-    package: WalletConnectProvider,
+    package: window.WalletConnectProvider.default,
     options: {
-      infuraId: "1e0153a074db4c0a88bdfb159f8ef93d"
+      rpc: {
+        56: "https://bsc-dataseed.binance.org/"
+      },
+      chainId: 56
     }
   }
 };
 
-const web3Modal = new Web3Modal({
+const web3Modal = new window.Web3Modal.default({
   cacheProvider: false,
   providerOptions
 });
-
-const presaleAddress = "0x3F7F7bC596D0494506993e38471894Af7bC41628";
-const abi = [{'inputs': [], 'name': 'buyTokens', 'outputs': [], 'stateMutability': 'payable', 'type': 'function'}, {'inputs': [], 'name': 'token', 'outputs': [{'internalType': 'address', 'name': '', 'type': 'address'}], 'stateMutability': 'view', 'type': 'function'}];
 
 async function connectWallet() {
   try {
@@ -41,17 +38,31 @@ async function buyTokens() {
     alert("Per favore connetti prima il wallet.");
     return;
   }
+
+  const presaleAddress = "0x3F7F7bC596D0494506993e38471894Af7bC41628";
+  const abi = [
+    {
+      "inputs": [],
+      "name": "buyTokens",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    }
+  ];
+
   const contract = new web3.eth.Contract(abi, presaleAddress);
-  const valueBNB = web3.utils.toWei("0.1", "ether"); // acquisto di esempio da 0.1 BNB
+  const input = document.getElementById("bnbAmount");
+  const valueBNB = web3.utils.toWei(input.value || "0", "ether");
+
   try {
-    const tx = await contract.methods.buyTokens().send({
+    await contract.methods.buyTokens().send({
       from: account,
       value: valueBNB
     });
-    alert("Acquisto riuscito!");
+    alert("Acquisto completato!");
   } catch (err) {
-    console.error("Errore durante l'acquisto", err);
-    alert("Errore durante l'acquisto. Riprova.");
+    console.error("Errore acquisto", err);
+    alert("Errore durante l'acquisto.");
   }
 }
 
